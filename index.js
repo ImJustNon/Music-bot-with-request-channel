3c00722fa5849f428572afec33d65a744647e192
ImJustNon <Im_Just_Non@hotmail.com>
2022-04-16 16:39:12 +0700

    commit to heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 //=============================== Config ========================================
 
 const prefix = '!';
-const token = ''; //
+const token = 'OTY0NDU4NjgxNTQ4ODIwNDkw.Ylk8JA.wv_hc6QdbYP09uZFGSMW0TGy8LI'; //
 const config = {
 	github: 'https://github.com/ImJustNon/Music-bot-with-request-channel',
 	mongoURL: 'mongodb://newuser:newuser@cluster0-shard-00-00.uf6th.mongodb.net:27017,cluster0-shard-00-01.uf6th.mongodb.net:27017,cluster0-shard-00-02.uf6th.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-6cm745-shard-0&authSource=admin&retryWrites=true&w=majority',
